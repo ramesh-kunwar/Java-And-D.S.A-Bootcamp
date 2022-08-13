e515d15891705d590551e9a16c7c8f70a99c1e66
ramesh-kunwar <rameshkunwar1110@gmail.com>
2022-08-13 19:08:07 +0545

    DSA project initialized

---
+++ src/TypeCasting.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // aoutomatic type promotion in expression
+//        int a = 257;
+//        byte b =(byte) a;
+//        System.out.println( a);
+
+//        byte a = 40;
+//        byte b=50;
+//        byte c =100;
+//        int d = (a*b)/c;
+//        System.out.println(d);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+}
