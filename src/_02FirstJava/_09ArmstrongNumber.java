c836eb4d8de566c59178b007bc8bae1c1ea2222b
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-20 19:07:17 +0545

    Armstrong file created

---
+++ src/_02FirstJava/_09ArmstrongNumber.java
@@ -0,0 +1,11 @@
+package _02FirstJava;
+
+import java.util.Scanner;
+
+public class _09ArmstrongNumber {
+    public static void main(String[] args) {
+//        To find Armstrong Number between two given number.
+
+
+    }
+}
